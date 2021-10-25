fe00f9f5e41136e81a20dabf4ca7f5bb43d26c16
ds1242 <david.shaw1242@gmail.com>
2021-10-25 13:47:50 -0600

    map loads with middle marker set onload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
