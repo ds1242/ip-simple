966d3643a33e37b2235d92958e249049cf2a6ba0
ds1242 <david.shaw1242@gmail.com>
2021-10-25 15:16:32 -0600

    added more styling to returned data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// const { data } = require("jquery");
+
 let mymap = L.map('mapid').setView([0,0], 1);
 let marker = L.marker([0,0]).addTo(mymap);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -15,7 +17,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const getGeoLoc = () => {
     let IP_API_KEY = 'at_yUzZqyZxjOKGmxpY3ajwM0wejSj6J';
     const geoApiUrl = 'https://geo.ipify.org/api/v2/country,city?apiKey=' + IP_API_KEY + '&ipAddress=';
-    console.log(geoApiUrl);
     
     // fetch(geoApiUrl)
     // .then(function(response){
@@ -23,15 +24,34 @@ const getGeoLoc = () => {
     // })
     // .then(function(data){
     //     console.log(data);
-    // // position = [data.location.lat, data.location.lng]
     //     mymap.flyTo(new L.LatLng(data.location.lat, data.location.lng), 13);
     //     marker.setLatLng([data.location.lat, data.location.lng]);
+
+    //     let addressDiv = document.getElementById('address');
+    //     addressDiv.append(data.ip)
+
+    //     let city = data.location.city;
+    //     let region = data.location.region;
+        // let zipcode = data.location.postalCode;
+    //     let locationDiv = document.getElementById('location');
+    //     locationDiv.append(city + ', ' + region + ' ' + zipcode);
+
+    //     let timeZone = data.location.timezone;
+    //     let timeZoneDiv = document.getElementById('timezone');
+    //     timeZoneDiv.append(timeZone);
+
+    //     let ISP = data.isp;
+    //     let ispDiv = document.getElementById('isp');
+    //     ispDiv.append(ISP);
+
+
     // })
     // .catch(function(error){
     //     console.log(error);
     // })
     let lat = 40.61106;
     let lng = -111.89994;
+    let ip = '192.212.174.101';
     let city = "Midvale";
     let region = "Utah";
     let timeZone = "-06:00";
@@ -40,13 +60,13 @@ const getGeoLoc = () => {
     marker.setLatLng([lat, lng]);
 
     let addressDiv = document.getElementById('address');
-    addressDiv.append(lat + ', ' + lng);
+    addressDiv.append(ip);
 
     let locationDiv = document.getElementById('location');
     locationDiv.append(city + ', ' + region);
 
     let timeZoneDiv = document.getElementById('timezone');
-    timeZoneDiv.append(timeZone);
+    timeZoneDiv.append('UTC ' + timeZone);
 
     let ispDiv = document.getElementById('isp');
     ispDiv.append(ISP);
