ef9918e6af81c7340648293d96dc9d71e56bcaa3
ds1242 <david.shaw1242@gmail.com>
2021-10-25 14:26:08 -0600

    use api to pull user location on load and zooms to their spot on the map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,35 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
+let mymap = L.map('mapid').setView([0,0], 1);
+let marker = L.marker([0,0]).addTo(mymap);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
 
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
 
 const getGeoLoc = () => {
     let IP_API_KEY = 'at_yUzZqyZxjOKGmxpY3ajwM0wejSj6J';
     const geoApiUrl = 'https://geo.ipify.org/api/v2/country,city?apiKey=' + IP_API_KEY + '&ipAddress=';
     console.log(geoApiUrl);
-    // fetch(geoApiUrl)
-    // .then(function(response){
-    //     return response.json();
-    // })
-    // .then(function(data){
-    //     console.log(data);
-    // // position = [data.location.lat, data.location.lng]
-    // })
-    // .catch(function(error){
-    //     console.log(error);
-    // })
+    
+    fetch(geoApiUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    // position = [data.location.lat, data.location.lng]
+        mymap.flyTo(new L.LatLng(data.location.lat, data.location.lng), 13);
+        marker.setLatLng([data.location.lat, data.location.lng]);
+    })
+    .catch(function(error){
+        console.log(error);
+    })
 };
 
 
@@ -37,3 +37,5 @@ window.onload = (event) => {
     console.log('page is fully loaded');
     getGeoLoc();
 };
+
+// mymap.on('click', getGeoLoc);
